5358f82f84221807c46a0406ecd5b716afbe8fad
Dulan Waduge <dulanwaduge1@gmail.com>
2023-11-03 12:44:46 +1100

    created intro, profile picture and animated, download cv button working

---
+++ components/intro.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Image from 'next/image'
+import React from 'react'
+import { motion, spring } from 'framer-motion'
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa';
+
+export default function Intro
+() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-50'>
+        <div className='flex items-center justify-center'>
+            <div className='relative'>
+                <motion.div
+                    initial={{opacity: 0, scale: 0}}
+                    animate={{opacity: 1, scale: 1 }}
+                    transition={{
+                        type: "tween",
+                        duration: 0.2
+                    }}>
+                    <Image src='/profilepicture.png' alt='profilepicture' 
+                        width={150} height={150} quality={95} priority={true}
+                        className='h-24 w-24 rounded-full border-[0.35rem] object-cover border-white shadow-xl'/>
+                </motion.div>
+                <motion.span className='absolute bottom-0 right-0 text-4xl'
+                    initial={{opacity: 0, scale:0}}
+                    animate={{opacity: 1, scale: 1}}
+                    transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        delay: 0.1,
+                        duration: 0.5,
+                    }}>
+                    👋
+                </motion.span>
+            </div>
+        </div>
+        
+        <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+                    initial={{opacity: 0, y:100}} 
+                    animate={{opacity: 1, y: 1}}>
+            <span className="font-bold">Hello, I'm Dulan.</span> I'm a{" "}
+            <span className="font-bold">graduate developer</span> looking to{" "}
+            <span className="font-bold">kickstart</span> my career. I enjoy
+                building <span className="italic">sites & apps</span>. My focus is{" "}
+            <span className="underline">full-stack development</span>.
+        </motion.p>
+
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium' 
+                    initial={{opacity: 0, y: 100}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{
+                        delay: 0.1,
+                    }}
+                >
+            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+                                                outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+                Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
+            </Link>
+            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 
+                             active:scale-105 transition cursor-pointer' href='/cv.pdf' download>
+                Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+            </a>
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 
+                            hover:scale-110 active:scale-105 transition'>
+                <BsLinkedin />
+            </a>
+            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full outline-none 
+                            focus:scale-110 hover:scale-110 active:scale-105 transition'>
+                <FaGithubSquare />
+            </a>
+        </motion.div>
+    </section>
+  )
+}
