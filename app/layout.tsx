24ab9b531d2a3bfdc9c40eeb9f2f06f268168e68
Dulan Waduge <dulanwaduge1@gmail.com>
2023-11-11 09:55:05 +1100

    minor adjustments to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 // Components
 import Header from '@/components/header'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className} bg-[#10142c] text-gray-300 relative h-[5000px] pt-28 sm:pt-36`}>
+      
         <div className='bg-[#10142c] 
               absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Projects from '@/components/projects';
 import SectionDivider from '@/components/section-divider';
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
